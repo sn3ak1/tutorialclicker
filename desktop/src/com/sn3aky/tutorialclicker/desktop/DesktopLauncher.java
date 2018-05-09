aed04e6b66a005da894960bdb336b07e564c5793
sn3ak1 <34197029+sn3ak1@users.noreply.github.com>
2018-05-09 21:45:38 +0200

    init

---
+++ desktop/src/com/sn3aky/tutorialclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sn3aky.tutorialclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sn3aky.tutorialclicker.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
