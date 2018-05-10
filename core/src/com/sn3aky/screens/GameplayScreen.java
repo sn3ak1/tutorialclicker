e810facc729e33c106397d93d7a8f769418f8e6e
sn3ak1 <34197029+sn3ak1@users.noreply.github.com>
2018-05-10 21:33:09 +0200

    added dummy GameplayScreen

---
+++ core/src/com/sn3aky/screens/GameplayScreen.java
@@ -0,0 +1,15 @@
+package com.sn3aky.screens;
+
+import com.sn3aky.tutorialclicker.TutorialClickerGame;
+
+public class GameplayScreen extends AbstractScreen {
+
+    public GameplayScreen(TutorialClickerGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+
+    }
+}
