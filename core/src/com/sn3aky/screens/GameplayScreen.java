9132a4bb3b03cf421a596ee449960b7c572c48ea
sn3ak1 <34197029+sn3ak1@users.noreply.github.com>
2018-05-10 22:30:21 +0200

    displaying Player image

--- core/src/com/sn3aky/screens/GameplayScreen.java
+++ core/src/com/sn3aky/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.sn3aky.screens;
 
+import com.sn3aky.entities.Player;
 import com.sn3aky.tutorialclicker.TutorialClickerGame;
 
 public class GameplayScreen extends AbstractScreen {
+
+    Player player;
 
     public GameplayScreen(TutorialClickerGame game) {
         super(game);
@@ -10,6 +13,25 @@ public class GameplayScreen extends AbstractScreen {
     }
 
     private void init() {
+        initPlayer();
+    }
 
+    private void initPlayer() {
+        player=new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
     }
 }
